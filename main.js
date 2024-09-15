62ecf03385de67f1b406f83d529f8cb44c88e774
Marc A. Donis <marc@balloonary.io>
2024-09-15 11:38:26 +0200

    .

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let isRecording = false;
 let audioChunks = [];
+let mediaRecorder;
 
 function load() {
 	document.getElementById('toggle-recording-button').innerHTML = 'Start';
@@ -12,24 +13,40 @@ async function toggleRecording() {
 		: 'Start';
 
 	if (isRecording) {
-		const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-
-		mediaRecorder = new MediaRecorder(stream);
-		mediaRecorder.ondataavailable = (event) => {
-			audioChunks.push(event.data);
-		};
-
-		const recordedAudio = document.getElementById('recorded-audio');
-		mediaRecorder.onstop = () => {
-			const audioBlob = new Blob(audioChunks, { type: 'audio/mpeg' });
-			const audioUrl = URL.createObjectURL(audioBlob);
-			console.log('audioUrl', audioUrl);
-			recordedAudio.src = audioUrl;
-			audioChunks = [];
-		};
-
-		mediaRecorder.start();
+		recordAudio();
 	} else {
 		mediaRecorder.stop();
+		const recordedAudio = document.getElementById('recorded-audio');
+		recordedAudio.addEventListener('ended', async () => {
+			console.log('ended');
+			mediaRecorder.stop();
+			await recordAudio();
+			recordedAudio.play();
+		});
+		setTimeout(() => {
+			recordedAudio.play();
+		}, 20);
 	}
+}
+
+async function recordAudio() {
+	const recordedAudio = document.getElementById('recorded-audio');
+	const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+	mediaRecorder = new MediaRecorder(stream);
+	mediaRecorder.ondataavailable = (event) => {
+		audioChunks.push(event.data);
+	};
+
+	mediaRecorder.onstop = async () => {
+		const audioBlob = new Blob(audioChunks, { type: 'audio/mpeg' });
+		// console.log('audioBlob.size', audioBlob.size);
+		// const arrayBuffer = await audioBlob.arrayBuffer();
+		const audioUrl = URL.createObjectURL(audioBlob);
+		// console.log('audioUrl', audioUrl);
+		recordedAudio.src = audioUrl;
+		audioChunks = [];
+	};
+
+	mediaRecorder.start();
 }
